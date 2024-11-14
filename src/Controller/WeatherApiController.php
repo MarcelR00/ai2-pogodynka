a0e10f6ad7e2478116acced529324a7611cd6a3f
Marcel Ragański <rm49378>
2024-11-14 09:57:22 +0100

    lab G

---
+++ src/Controller/WeatherApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Measurement;
+use App\Service\WeatherUtil;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\Routing\Attribute\Route;
+
+class WeatherApiController extends AbstractController
+{
+    #[Route('/api/v1/weather', name: 'app_weather_api')]
+    public function index(
+        WeatherUtil $util,
+        #[MapQueryParameter('country')] string $country,
+        #[MapQueryParameter('city')] string $city,
+        #[MapQueryParameter('format')] string $format = 'json',
+    ): JsonResponse
+    {
+        $measurements = $util->getWeatherForCountryAndCity($country, $city);
+        return $this->json([
+            'city' => $city,
+            'country' => $country,
+            'measurements' => array_map(fn(Measurement $m) => [
+                'date' => $m->getDate()->format('Y-m-d'),
+                'celsius' => $m->getCelsius(),
+            ], $measurements),
+        ]);
+    }
+}
